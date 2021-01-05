15ec4e9704237c89a074d4fea71ccd66d28bc7d3
Ali Spittel <ali07cat07@gmail.com>
2021-01-05 09:53:28 -0600

    Update PostList.js

--- src/PostList.js
+++ src/PostList.js
@@ -1,5 +1,4 @@
 import { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
 import { DataStore } from '@aws-amplify/datastore'
 
 import { Post } from './models'
@@ -15,14 +14,12 @@ export default function PostList () {
   }, [])
 
   return (
-    <div className='tile is-ancestor'>
+    <div>
       {posts.map(post => (
-        <div className='tile is-parent' key={post.id}>
-          <section className='tile is-child notification box is-info'>
-            <Link to={`/post/${post.id}`}>
-              <h2 className='title is-4'>{post.title}</h2>
-            </Link>
-            <p className='content'>{post.content.substring(0, 300)}...</p>
+        <div key={post.id}>
+          <section>
+            <h2>{post.title}</h2>
+            <p>{post.content.substring(0, 300)}...</p>
           </section>
         </div>
       ))}
